61269227ee09f855050fafb9b3b712bdbd702010
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-03-02 14:52:41 -0500

    Update ModalContentProvider.js

--- src/App/UI/ModalContentProvider.js
+++ src/App/UI/ModalContentProvider.js
@@ -5,12 +5,13 @@ export default class ModalContentProvider {
     this.modalContents = {
       aboutMe: {
         title: 'About me',
-        picture: '/Assets/HighTop.jpeg',
+        imageUrl: '/App/Assets/HighTop.jpeg',
         description: 'A highly ambitious young man committed to achieving excellence at the highest level. Recognizes the significance of diligence, unshakeable commitment, and above all, fostering strong bonds with others. Prefers leading through actions rather than mere words. Diligently conducts research to refine ideas and broaden perspectives, actively seeking guidance from mentors and industry trailblazers. Holds integrity in the highest regard—considered one of his most cherished traits. Takes words seriously, ensuring accountability for actions and readily acknowledging and learning from mistakes.',
       },
       projects: {
         title: 'React Fullstack Web Application',
-        description: 'Architected an image recognition app with machine learning API in React. Enhanced skills in ML integration, front-end development, and API handling. Proactively tackled challenges, ensuring seamless synergy between AI and user interface. Web App URL: https://mindgames-y5b6.onrender.com ',
+        imageUrl: "/App/Assets/FRB.jpeg",
+        description: 'Architected an image recognition app with machine learning API in React. Enhanced skills in ML integration, front-end development, and API handling. Proactively tackled challenges, ensuring seamless synergy between AI and user interface. Web App URL: <a href="https://mindgames-y5b6.onrender.com">MindGamesApp</a>',
       },
       contactMe: {
         title: 'Contact Me',
