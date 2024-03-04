6673718b8564be19e03e33bdc3ee280b58968b51
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-03-04 15:58:19 -0500

    Update ModalManager.js

--- src/App/UI/ModalManager.js
+++ src/App/UI/ModalManager.js
@@ -1,25 +1,29 @@
 export default class ModalManager {
     constructor() {
-      this.modal = document.getElementById("myModal");
-      this.close = document.getElementsByClassName("close")[0];
-      this.close.onclick = () => {
-        this.closeModal()
-      }
+        this.modal = document.getElementById("myModal");
+        this.modalImage = document.getElementById("modalImage"); // Define modalImage
+        this.close = document.getElementsByClassName("close")[0];
+        this.close.onclick = () => {
+            this.closeModal()
+        }
     }
   
-    openModal(title, description) {
-      document.getElementById("modalTitle").innerHTML = title;
-      document.getElementById("modalDescription").innerHTML = description;
-      this.modal.style.display = "block";
-      this.modal.classList.remove('fadeOut');
-      this.modal.classList.add('fadeIn');
+    openModal(title, description, imageUrl) {
+        document.getElementById("modalTitle").innerHTML = title;
+        document.getElementById("modalDescription").innerHTML = description;
+        this.modalImage.src = "/App/Assets/HighTop.jpeg"; // Set the src attribute to the provided imageUrl
+        this.modal.style.display = "block";
+        this.modal.classList.remove('fadeOut');
+        this.modal.classList.add('fadeIn');
     }
+    
+  
   
     closeModal() {
-      this.modal.classList.remove('fadeIn');
-      this.modal.classList.add('fadeOut');
-      setTimeout(() => {
-        this.modal.style.display = "none";
-      }, 600);
+        this.modal.classList.remove('fadeIn');
+        this.modal.classList.add('fadeOut');
+        setTimeout(() => {
+            this.modal.style.display = "none";
+        }, 600);
     }
   }
