dcfea4b95eeadc17341a6a61c2ed9208fd038d82
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-03-07 17:33:13 -0500

    Update Environment.js

--- src/App/World/Environment.js
+++ src/App/World/Environment.js
@@ -1,78 +1,102 @@
 import * as THREE from "three";
-import App from "../App";
-import ModalManager from "../UI/ModalManager";
+import App from "../App.js";
+import assetStore from "../Utils/AssetStore.js";
+import Portal from "./Portal.js";
+import ModalContentProvider from "../UI/ModalContentProvider.js";
 
-export default class Portal {
-    constructor(portalMesh, modalInfo) {
-        this.app = new App();
-        this.portalMesh = portalMesh;
-        this.modalInfo = modalInfo;
-        this.modalManager = new ModalManager();
+export default class Environment {
+  constructor() {
+    this.app = new App();
+    this.scene = this.app.scene;
+    this.physics = this.app.world.physics;
+    this.pane = this.app.gui.pane;
 
-        this.portalNearMaterial = new THREE.MeshBasicMaterial({
-            color: 0xFFFFFF,
-            transparent: true,
-            opacity: 0.8,
-        });
+    this.assetStore = assetStore.getState()
+    this.environment = this.assetStore.loadedAssets.environment
 
-        this.portalFarMaterial = new THREE.MeshBasicMaterial({
-            color: 0x00FFFF,
-            transparent: true,
-            opacity: 0.8,
-        });
+    this.loadEnvironment();
+    this.addLights();
+    this.addPortals();
+  }
 
-        this.portalMesh.material = this.portalFarMaterial;
+  loadEnvironment() {
+    // load environment here
+    const environmentScene = this.environment.scene;
+    this.scene.add(environmentScene);
 
-        this.prevIsNear = false;
-    }
+    environmentScene.position.set(-4.8, 0, -7.4)
+    environmentScene.rotation.set(0, -.60, 0)
+    environmentScene.scale.setScalar(1.3)
 
-    loop() {
-        this.character = this.app.world.character.instance;
-        if (this.character) {
-            const portalPosition = new THREE.Vector3();
-            this.portalMesh.getWorldPosition(portalPosition);
+    const physicalObjects = ['trees',
+      'terrain',
+      'rocks',
+      'stairs',
+      'gates',
+      'floor',
+      'bushes'
+    ]
 
-            const distance = this.character.position.distanceTo(portalPosition);
-            const isNear = distance < 1.5;
-            if (isNear) {
-                if (!this.prevIsNear){
-                    // Open modal with portal information
-                    const MAX_IMAGE_WIDTH = 350; // Adjust as per your requirements
-                    const MAX_IMAGE_HEIGHT = 350; // Adjust as per your requirements
-                    console.log(MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT)
+    const shadowCasters = ['trees',
+      'terrain',
+      'rocks',
+      'stairs',
+      'gates',
+      'bushes'
+    ]
 
-                    const image = new Image();
-                    image.onload = () => {
-                        const aspectRatio = image.width / image.height;
-                        let scaledWidth = image.width;
-                        let scaledHeight = image.height;
+    const shadowReceivers = ['floor',
+      'terrain'
+    ]
 
-                        if (scaledWidth > MAX_IMAGE_WIDTH) {
-                            scaledWidth = MAX_IMAGE_WIDTH;
-                            scaledHeight = scaledWidth / aspectRatio;
-                        }
-
-                        if (scaledHeight > MAX_IMAGE_HEIGHT) {
-                            scaledHeight = MAX_IMAGE_HEIGHT;
-                            scaledWidth = scaledHeight * aspectRatio;
-                        }
-
-                        // Update modal with scaled image
-                        this.modalManager.openModal(this.modalInfo.title, this.modalInfo.description, this.modalInfo.imageUrl, scaledWidth, scaledHeight);
-                    };
-
-                    image.src = this.modalInfo.imageUrl;
-                    this.portalMesh.material = this.portalNearMaterial;
-                }
-                this.prevIsNear = true;
-            } else {
-                if (this.prevIsNear) {
-                    // Close modal and revert portal material
-                    this.modalManager.closeModal();
-                    this.portalMesh.material = this.portalFarMaterial;
-                }
-                this.prevIsNear = false;
-            }
+    for (const child of environmentScene.children) {
+      child.traverse((obj) => {
+        if (obj.isMesh) {
+          obj.castShadow = shadowCasters.some((keyword) => child.name.includes(keyword))
+          obj.receiveShadow = shadowReceivers.some((keyword) => child.name.includes(keyword))
+          if (physicalObjects.some((keyword) => child.name.includes(keyword))) {
+            this.physics.add(obj, "fixed", "cuboid")
+          }
         }
+      })
     }
+  }
+
+
+  addLights() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    this.scene.add(ambientLight);
+
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    this.directionalLight.position.set(1, 1, 1);
+    this.directionalLight.castShadow = true;
+    this.directionalLight.shadow.camera.top = 30
+    this.directionalLight.shadow.camera.right = 30
+    this.directionalLight.shadow.camera.left = -30
+    this.directionalLight.shadow.camera.bottom = -30
+    this.directionalLight.shadow.bias = -0.002
+    this.directionalLight.shadow.normalBias = 0.072
+    this.scene.add(this.directionalLight);
+  }
+
+  addPortals() {
+
+    const portalMesh1 = this.environment.scene.getObjectByName('portals')
+    const portalMesh2 = this.environment.scene.getObjectByName('portals001')
+    const portalMesh3 = this.environment.scene.getObjectByName('portals002')
+
+    const modalContentProvider = new ModalContentProvider()
+
+
+    this.portal1 = new Portal(portalMesh1, modalContentProvider.getModalInfo('aboutMe'))
+    this.portal2 = new Portal(portalMesh2, modalContentProvider.getModalInfo('projects'))
+    this.portal3 = new Portal(portalMesh3, modalContentProvider.getModalInfo('contactMe'))
+  }
+
+  loop() {
+    this.portal1.loop()
+    this.portal2.loop()
+    this.portal3.loop()
+  }
+
 }
