a951f54e228a9c43fddb999920cc934b00d601f7
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-03-13 11:56:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default {
-    root: 'src/',
+    root: 'dist/',
     publicDir: '../static/',
     base: './',
     plugins: [
